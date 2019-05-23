88ad3c94d88e565f1d56b508262584115fa6190a
Федина Юлия <yu.belova@2gis.ru>
2019-05-23 16:28:18 +0700

    задание 1

--- Lecture1/Program.cs
+++ Lecture1/Program.cs
@@ -6,7 +6,22 @@ namespace Lecture1
     {
         static void Main(string[] args)
         {
-
+            do
+            {
+                Console.WriteLine("Введите первое число");
+                int input1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Введите второе число");
+                int input2 = Convert.ToInt32(Console.ReadLine());
+                try
+                {
+                    int result = input1 + input2;
+                    Console.WriteLine(input1 + " + " + input2 + " = " + result);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
         }
     }
 }
